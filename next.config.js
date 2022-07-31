117ebbd547d2cb4a5a1fef09b30a0fc6a17b9d0e
anurag-shuklaECIS <anurag.shukla@ecinfosolutions.com>
2022-07-31 20:47:43 +0530

    commit

--- next.config.js
+++ next.config.js
@@ -7,12 +7,6 @@ module.exports = {
 
     return config;
   },
+  target: 'serverless'
 };
 
-const withPlugins = require('next-compose-plugins')
-const withImages = require('next-images')
-const withVideos = require('next-videos')
-
-module.exports = withPlugins([withImages, withVideos], {
-  target: 'serverless',
-})
