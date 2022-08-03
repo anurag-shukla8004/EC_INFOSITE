8079dfd9f97c6c4eb92c2cea632c119c23043b52
anurag-shuklaECIS <anurag.shukla@ecinfosolutions.com>
2022-08-03 18:22:55 +0530

    commit

--- next.config.js
+++ next.config.js
@@ -1,18 +1,4 @@
-if (!process.env.WORDPRESS_API_URL) {
-  throw new Error(`
-    Please provide a valid WordPress instance URL.
-    Add to your environment variables WORDPRESS_API_URL.
-  `)
-}
-
 module.exports = {
-  images: {
-    domains: [
-     'ecinfosolutions.com',
-      '2.gravatar.com',
-      'secure.gravatar.com',
-    ],
-  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
@@ -21,10 +7,4 @@ module.exports = {
 
     return config;
   },
-  target: 'serverless'
 };
-
-
-
-
-
