7a2c1b49a5ea247441b20d4cf8d18c189960a60b
anurag-shuklaECIS <anurag.shukla@ecinfosolutions.com>
2022-08-12 12:00:03 +0530

    commit

--- next.config.js
+++ next.config.js
@@ -18,14 +18,14 @@ module.exports = {
      'eclocalwebsite.local',
     ],
   },
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: [`@svgr/webpack`],
-    });
+  // webpack(config) {
+  //   config.module.rules.push({
+  //     test: /\.svg$/,
+  //     use: [`@svgr/webpack`],
+  //   });
 
-    return config;
-  },
+  //   return config;
+  // },
     target: 'serverless'
 };
 
