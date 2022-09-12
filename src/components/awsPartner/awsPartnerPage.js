cab6613084f68891600d69aefa41fb5f1d3bb5f4
anurag-shuklaECIS <anurag.shukla@ecinfosolutions.com>
2022-09-12 13:08:43 +0530

    commit

--- src/components/awsPartner/awsPartnerPage.js
+++ src/components/awsPartner/awsPartnerPage.js
@@ -50,7 +50,7 @@ function pytDevPage({}) {
   useEffect(() => {
     async function getUser() {
       try {
-        const response = await fetch('http://43.205.94.230/api/aws_cloud');
+        const response = await fetch('http://43.205.94.230:8000/api/aws_cloud');
 
         if (!response.ok) {
           throw new Error(`Error! status: ${response.status}`);
