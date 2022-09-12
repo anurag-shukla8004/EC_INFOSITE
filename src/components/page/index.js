ad3d7235a024eacc79b13f739b0b46d883309d14
anurag-shuklaECIS <anurag.shukla@ecinfosolutions.com>
2022-09-12 11:59:10 +0530

    commit

--- src/components/page/index.js
+++ src/components/page/index.js
@@ -5,15 +5,15 @@ import Navigation from '../../components/navigation/index';
 const Page = ({ children }) => (
   <div>
     <Head>
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"/> 
       <link rel="icon" href="./ecLogo2.png" />
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" />
-      
       <link
         href="https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,100;0,200;0,400;0,500;0,600;0,700;0,800;0,900;1,300&family=Roboto:wght@300;500&display=swap"
         rel="stylesheet"
       />
-      <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"/> 
+      
     </Head>
     <div className={tw(`min-h-screen flex flex-col`)}>
       <Navigation />
